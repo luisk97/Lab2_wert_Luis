f5d13f54644df2fe73510f4b7344cf22e84a81f8
Luis Mora <luisk@estudiantec.cr>
2022-04-16 06:58:52 -0600

    Cambio en el script de python

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -1,2 +1,31 @@
+import os
+import tempfile
+
+from google.cloud import storage, vision
+from wand.image import Image
+
+storage_client = storage.Client()
+client = vision.ImageAnnotatorClient()
+
 def main(event, context):
     print("Evento-contexto", context)
+
+    #client = vision.ImageAnnotatorClient()
+
+    image_to_open = 'cara.jpg'
+
+    with open(image_to_open, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    face_response = client.face_detection(image=image)
+
+    #face_data = []
+
+    for face_detection in face_response.face_annotations:
+        d={
+            'confidence': face_detection.detection_confidence,
+            'joy': face_detection.joy_likelihood
+        }
+        print(d)
